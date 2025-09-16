6e615ef778e2bab58e7508d98e47cab33850ee77
Poncho Villarreal <svilla00@uafs.edu>
2025-09-16 10:55:10 -0500

    I forgor x2

--- sept16.c
+++ sept16.c
@@ -11,6 +11,9 @@ int main(){
     //array[0] = 10; would *also* do that. Memory allocated on heap can be *navigated* using [], but not *initialized*.
     //array++;    This
     //array = 20; and this together would allocate 20 to the 2nd location in the heap array. 
+    //*(array + 1) = 20; for some reason would *also* allocate it that way. Make sure to ask why.
+    // Hightower says that it's the same thing as the thing as array[0] = 10;
+    // After allocating the size of the array to the heap, you can just use standard array notation to work with the information instead of having to do a whole lot of other junk. He also suggests that we do the same thing.
 
     for(i = 0; i < 5; i++){
         num[i] = val;
